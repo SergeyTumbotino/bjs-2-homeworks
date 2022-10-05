9a5c52d1e6d91505ca9d1a8469ca1f413d2f7238
Sergey Novikov <novsp78@gmail.com>
2022-10-06 00:59:48 +0300

    2.functions

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -3,29 +3,62 @@ function getArrayParams(arr) {
   let min, max, sum, avg;
 
   // Ваш код
-
+  max = -101;
+  min = 101;
+  sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    sum += arr[i];
+  }
+  avg = sum / arr.length;
+  avg = Number(avg.toFixed(2));
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
 
   // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = -Infinity;
+  let funcResult;
 
   // Ваш кода
-  // for ...
-  
+  for (let i = 0; i < arrOfArr.length; i++) {
+    funcResult = func(arrOfArr[i]);
+    if (max < funcResult) {
+      max = funcResult;
+    }
+  }
+
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
   // Ваш код
+  let max = -Infinity;
+  let min = Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+  return Math.abs(max - min);
 }
