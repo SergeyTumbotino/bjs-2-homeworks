260331abcc3d7dcb1b246e51dc9422d616a19ae0
Sergey Novikov <novsp78@gmail.com>
2022-10-08 15:06:32 +0300

    arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -2,6 +2,10 @@ function compareArrays(arr1, arr2) {
   let result;
 
   // Ваш код
+  arr1.length >= arr2.length
+    ? ((arrFirst = arr1), (arrSecond = arr2))
+    : ((arrFirst = arr2), (arrSecond = arr1));
+  result = arrFirst.every((elem, index) => elem === arrSecond[index]);
 
   return result; // boolean
 }
@@ -10,6 +14,10 @@ function advancedFilter(arr) {
   let resultArr;
 
   // Ваш код
+  resultArr = arr
+    .filter((elem) => elem > 0)
+    .filter((elem) => elem % 3 === 0)
+    .map((elem) => elem * 10);
 
   return resultArr; // array
 }
